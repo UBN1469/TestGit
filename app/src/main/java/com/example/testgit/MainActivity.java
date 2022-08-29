a8a2b4a36e28b838fbc597fc548b912e17eca383
UBN_GK <5423410@mail.ru>
2022-08-29 11:39:41 +0300

    Commit from android studio2

--- app/src/main/java/com/example/testgit/MainActivity.java
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
@@ -18,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                Toast.makeText(MainActivity.this,"Hello", Toast.LENGTH_SHORT).show();
+
             }
         });
     }
